202c10d4facc5cf4c8923763862f2235257eacc0
andremadu <andremm2412@gmail.com>
2025-02-20 15:53:32 +0000

    Criando automaticamente test.stories.tsx

---
+++ src/components/test/test.stories.tsx
@@ -0,0 +1,15 @@
+import test from "./test";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta: Meta<typeof test> = {
+  title: "Componentes/test",
+  component: test,
+  argTypes: {},
+};
+
+export default meta;
+type Story = StoryObj<typeof test>;
+
+export const Default: Story = {
+  args: {},
+};
